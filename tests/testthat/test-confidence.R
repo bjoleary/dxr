31d379aa7aa0b09219ad020d4b1908dba2bad238
Brendan O'Leary <brendan@boleary.com>
2021-09-26 16:41:44 -0400

    Add tests for confidence function

--- tests/testthat/test-confidence.R
+++ tests/testthat/test-confidence.R
@@ -1,3 +1,35 @@
 test_that("confidence works", {
-  testthat::skip("Need to write this test")
+  # Worked example: large numbers (Altman loc 1189)
+  expect_equal(
+    object =
+      confidence(
+        true_calls = 81,
+        false_calls = 263 - 81,
+        interval = 0.95
+      ),
+    expected =
+      list(lower_bound = 0.255288519878274, upper_bound = 0.3662095769828)
+  )
+
+  # Worked examples: small numbers (Altman loc 1189)
+  expect_equal(
+    object =
+      confidence(
+        true_calls = 1,
+        false_calls = 28,
+        interval = 0.95
+      ),
+    expected =
+      list(lower_bound = 0.00611321429276265, upper_bound = 0.171755218793203)
+  )
+  expect_equal(
+    object =
+      confidence(
+        true_calls = 0,
+        false_calls = 20,
+        interval = 0.95
+      ),
+    expected =
+      list(lower_bound = 0, upper_bound = 0.161125158052819)
+  )
 })
