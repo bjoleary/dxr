17d8aadb08a47d1007e166b94ac38ce2b36d34e4
Brendan O'Leary <brendan@boleary.com>
2021-10-07 01:06:13 -0400

    Get test eval info from an existing evaluation

--- tests/testthat/helper-full_evaluations.R
+++ tests/testthat/helper-full_evaluations.R
@@ -1,6 +1,40 @@
 url_root <- "https://www.accessdata.fda.gov/cdrh_docs/presentations/maf/"
 # A Panel 1 evaluation:
-ex_eval <- readr::read_csv(file = paste0(url_root, "maf3246-a001.csv" ))
+ex_eval <-
+  readr::read_csv(
+    file = paste0(url_root, "maf3246-a001.csv" ),
+    col_types =
+      readr::cols(
+        manufacturer = readr::col_character(),
+        device = readr::col_character(),
+        date_performed = readr::col_date(format = ""),
+        lot_number = readr::col_character(),
+        panel = readr::col_character(),
+        sample_no = readr::col_double(),
+        sample_id = readr::col_character(),
+        type = readr::col_character(),
+        group = readr::col_character(),
+        days_from_symptom = readr::col_double(),
+        igm_result = readr::col_logical(),
+        igg_result = readr::col_character(),
+        iga_result = readr::col_logical(),
+        pan_result = readr::col_logical(),
+        igm_igg_result = readr::col_logical(),
+        control = readr::col_character(),
+        pan_titer = readr::col_double(),
+        igm_titer = readr::col_double(),
+        igg_titer = readr::col_double(),
+        igm_truth = readr::col_character(),
+        igg_truth = readr::col_character(),
+        antibody_truth = readr::col_character(),
+        igm_agree = readr::col_logical(),
+        igg_agree = readr::col_character(),
+        iga_agree = readr::col_logical(),
+        pan_agree = readr::col_logical(),
+        igm_igg_agree = readr::col_logical(),
+        antibody_agree = readr::col_character()
+      )
+    )
 
 nci_sero_panel_1_details <-
   ex_eval %>%
@@ -92,3 +126,24 @@ nci_1$panel_table <-
           "Collected prior to 2020"
       )
   )
+
+suppressWarnings(
+  test_eval <-
+    build_evaluation_sheet(
+      evaluation_name = "Test of NCI Panel 1 Evaluation",
+      evaluation_description = "Nothing",
+      developer = ex_eval$manufacturer %>% unique(),
+      assay = ex_eval$device %>% unique(),
+      lot_numbers = ex_eval$lot_number %>% unique(),
+      panel_data = nci_1,
+      panel_data_filepath = NA_character_,
+      analytes = "IgG",
+      targets = "Spike",
+      qualitative_outcomes = c("Positive", "Borderline", "Negative"),
+      semiquantitative_outcomes = NA_character_,
+      quantitative_units = NA_character_,
+      randomize = TRUE,
+      blind = TRUE
+    )
+)
+
