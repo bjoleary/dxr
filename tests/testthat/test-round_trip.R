93f32c2b19ea4966001ad925f3e88f9fb85d45b5
Brendan O'Leary <brendan@boleary.com>
2021-10-11 11:00:08 -0400

    Initial version of functions to roundtrip an evaluation to Excel
    We can now write evaluation data to excel and read it back in.
    
    Test cases do not include any data though, so still much to do here.

--- tests/testthat/test-round_trip.R
+++ tests/testthat/test-round_trip.R
@@ -86,3 +86,56 @@ test_that("round-tripping nci_1 works", {
     expected = nci_1
   )
 })
+
+test_that("writing and reading an evaluation works", {
+  # Create a temporary excel workbook file -------------------------------------
+  filepath_workbook <- tempfile(fileext = ".xlsx")
+  # Save and write some panel sheet data ---------------------------------------
+  eval_data_sent <-
+    build_evaluation_sheet(
+      evaluation_name = "Example Evaluation",
+      evaluation_description = NA_character_,
+      developer = "ACME Test Corp.",
+      assay = "Test Assay #1",
+      lot_numbers = "20200101",
+      panel_data =
+        build_panel_sheet(
+          panel_name = "Example Panel",
+          panel_description = "An example panel.",
+          n_samples = 5L,
+          sample_groups = "Samples",
+          sample_matrices = "Serum",
+          analytes = c("IgM", "IgG", "Pan-Ig"),
+          targets = "Spike",
+          qualitative_outcomes = c("Positive", "Negative"),
+          qualitative_comparators = "Authorized NAAT and CDC Assay",
+          semiquantitative_outcomes = NA,
+          semiquantitative_comparators = NA,
+          quantitative_units = NA,
+          quantitative_comparators = NA
+        ),
+      analytes = c("IgM", "IgG", "Pan-Ig"),
+      targets = "Spike",
+      qualitative_outcomes = c("Positive", "Negative"),
+      semiquantitative_outcomes = NA_character_,
+      quantitative_units = NA_character_,
+      randomize = FALSE,
+      blind = FALSE
+    )
+  # Write evaluation -----------------------------------------------------------
+  write_evaluation_sheet(
+    evaluation_sheet_data = eval_data_sent,
+    filepath = filepath_workbook,
+    method = "excel"
+  )
+  # Read that file -------------------------------------------------------------
+  eval_data_returned <-
+    read_evaluation(
+      filepath = filepath_workbook
+    )
+
+  expect_equal(
+    object = eval_data_returned,
+    expected = eval_data_sent
+  )
+})
