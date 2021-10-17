9354eabfd1d24ff3ac6dd926053e6d88f8cfd258
Brendan O'Leary <brendan@boleary.com>
2021-10-17 11:17:29 -0400

    Improve testing of evaluation scoring

--- tests/testthat/test-score_evaluation.R
+++ tests/testthat/test-score_evaluation.R
@@ -1,6 +1,41 @@
 test_that("scoring evaluations works", {
-  skip("TODO")
-  # We'll want to round-trip a known evaluation and check the counts.
+  test_eval_outcomes <-
+    score_evaluation(
+      panel_data = nci_1,
+      evaluation_data = test_eval
+    )
+  expect_equal(
+    object =  test_eval_outcomes$qualitative_match %>% sum(),
+    expected = 107
+  )
+  expect_equal(
+    object =
+      test_eval_outcomes$qualitative_outcome_strict %>%
+      stringr::str_detect("True Negative") %>%
+      sum(),
+    expected = 80
+  )
+  expect_equal(
+    object =
+      test_eval_outcomes$qualitative_outcome_strict %>%
+      stringr::str_detect("True Positive") %>%
+      sum(),
+    expected = 27
+  )
+  expect_equal(
+    object =
+      test_eval_outcomes$qualitative_outcome_strict %>%
+      stringr::str_detect("False Negative") %>%
+      sum(),
+    expected = 3
+  )
+  expect_equal(
+    object =
+      test_eval_outcomes$qualitative_outcome_strict %>%
+      stringr::str_detect("False Positive") %>%
+      sum(),
+    expected = 0
+  )
 })
 
 test_that("score an example evaluation", {
