8f1ee43ffdd0962f8acc8b74b3f3729d3a7b7ff2
Brendan O'Leary <brendan@boleary.com>
2021-10-30 21:42:02 -0400

    Enable input validation in excel for evaluation sheets

--- R/write_evaluation_sheet.R
+++ R/write_evaluation_sheet.R
@@ -384,7 +384,310 @@ evaluation_sheet_excel_method <- function(evaluation_sheet_data) {
     lockObjects = TRUE,
     lockScenarios = TRUE
   )
+  # Input validation -----------------------------------------------------------
+  ## Set up the data -----------------------------------------------------------
+  # Not. Elegant. Don't judge, okay?
+  # Make a vector for each input to validate
+  analyte_validation <-
+    metadata$analytes %>%
+    unlist()
+  target_validation <-
+    metadata$targets %>%
+    unlist()
+  lot_validation <-
+    metadata$lot_numbers %>%
+    unlist()
+  qual_result_validation <-
+    metadata$qualitative_outcomes %>%
+    unlist()
+  semi_result_validation <-
+    metadata$semiquantitative_outcomes %>%
+    unlist()
+  quant_units_validation <-
+    metadata$quantitative_units %>%
+    unlist()
 
+  # Find the length of the longest vector
+  longest <-
+    max(
+      length(analyte_validation),
+      length(target_validation),
+      length(lot_validation),
+      length(qual_result_validation),
+      length(semi_result_validation),
+      length(quant_units_validation)
+    )
+
+  # Bind a lengthened version of each of those vectors as a column in the
+  # new input_validation table
+  input_validation <-
+    dplyr::bind_cols(
+      analyte_validation = lengthen(analyte_validation, longest),
+      target_validation = lengthen(target_validation, longest),
+      lot_validation = lengthen(lot_validation, longest),
+      qual_result_validation = lengthen(qual_result_validation, longest),
+      semi_result_validation = lengthen(semi_result_validation, longest),
+      quant_units_validation = lengthen(quant_units_validation, longest)
+    )
+
+  ## Write the table to a new sheet --------------------------------------------
+  openxlsx::addWorksheet(
+    wb = workbook,
+    sheetName = "input_validation",
+    tabColour = "red",
+    header =
+      c(
+        # Left side header:
+        "",
+        # Center header:
+        paste(
+          evaluation_sheet_data$evaluation_metadata$value[[1]],
+          "Input Validation"
+        ),
+        # Right side header
+        ""
+      ),
+    footer =
+      c(
+        # Left side footer:
+        "",
+        # Center footer:
+        "Page &[Page] of &[Pages]",
+        # Right side footer
+        ""
+      ),
+    paperSize = 1, # Letter paper
+    orientation = "landscape",
+    gridLines = FALSE,
+    visible = FALSE
+  )
+  openxlsx::writeData(
+    wb = workbook,
+    sheet = "input_validation",
+    x = input_validation,
+    startCol = 1,
+    startRow = 1,
+    colNames = TRUE,
+    rowNames = FALSE,
+    keepNA = FALSE,
+    na.string = ""
+  )
+  ## Apply input validation ----------------------------------------------------
+  # TODO: No doubt this could be a map function instead.
+  ### Analyte ------------------------------------------------------------------
+  suppressWarnings(
+    openxlsx::dataValidation(
+      wb = workbook,
+      sheet = "evaluation_data",
+      cols =
+        which(colnames(evaluation_sheet_data$evaluation_table) == "analyte"),
+      rows = seq_along(evaluation_sheet_data$evaluation_table$sample) + 1,
+      type = "list",
+      operator = "between", # ignored
+      value =
+        validation_string(
+          input_validation = input_validation,
+          column_vector = analyte_validation
+        ),
+      allowBlank = TRUE,
+      showInputMsg = TRUE,
+      showErrorMsg = TRUE
+    )
+  )
+
+  ### Target -------------------------------------------------------------------
+  suppressWarnings(
+    openxlsx::dataValidation(
+      wb = workbook,
+      sheet = "evaluation_data",
+      cols =
+        which(colnames(evaluation_sheet_data$evaluation_table) == "target"),
+      rows = seq_along(evaluation_sheet_data$evaluation_table$sample) + 1,
+      type = "list",
+      operator = "between", # ignored
+      value =
+        validation_string(
+          input_validation = input_validation,
+          column_vector = target_validation
+        ),
+      allowBlank = TRUE,
+      showInputMsg = TRUE,
+      showErrorMsg = TRUE
+    )
+  )
+
+  ### Lot ----------------------------------------------------------------------
+  suppressWarnings(
+    openxlsx::dataValidation(
+      wb = workbook,
+      sheet = "evaluation_data",
+      cols =
+        which(colnames(evaluation_sheet_data$evaluation_table) == "lot_number"),
+      rows = seq_along(evaluation_sheet_data$evaluation_table$sample) + 1,
+      type = "list",
+      operator = "between", # ignored
+      value =
+        validation_string(
+          input_validation = input_validation,
+          column_vector = lot_validation
+        ),
+      allowBlank = TRUE,
+      showInputMsg = TRUE,
+      showErrorMsg = TRUE
+    )
+  )
+
+  ### Qualitative Result -------------------------------------------------------
+  suppressWarnings(
+    openxlsx::dataValidation(
+      wb = workbook,
+      sheet = "evaluation_data",
+      cols =
+        which(
+          colnames(evaluation_sheet_data$evaluation_table) ==
+            "qualitative_result"
+        ),
+      rows = seq_along(evaluation_sheet_data$evaluation_table$sample) + 1,
+      type = "list",
+      operator = "between", # ignored
+      value =
+        validation_string(
+          input_validation = input_validation,
+          column_vector = qual_result_validation
+        ),
+      allowBlank = TRUE,
+      showInputMsg = TRUE,
+      showErrorMsg = TRUE
+    )
+  )
+
+  ### Semi Quantitative Result -------------------------------------------------
+  if (
+    "semiquantitative_result" %in%
+      colnames(evaluation_sheet_data$evaluation_table)
+    ) {
+    suppressWarnings(
+      openxlsx::dataValidation(
+        wb = workbook,
+        sheet = "evaluation_data",
+        cols =
+          which(
+            colnames(evaluation_sheet_data$evaluation_table) ==
+              "semiquantitative_result"
+          ),
+        rows = seq_along(evaluation_sheet_data$evaluation_table$sample) + 1,
+        type = "list",
+        operator = "between", # ignored
+        value =
+          validation_string(
+            input_validation = input_validation,
+            column_vector = semi_result_validation
+          ),
+        allowBlank = TRUE,
+        showInputMsg = TRUE,
+        showErrorMsg = TRUE
+      )
+    )
+  }
+
+  ### Quantitative Units -------------------------------------------------------
+  if (
+    "quantitative_units" %in%
+      colnames(evaluation_sheet_data$evaluation_table)
+  ) {
+    suppressWarnings(
+      openxlsx::dataValidation(
+        wb = workbook,
+        sheet = "evaluation_data",
+        cols =
+          which(
+            colnames(evaluation_sheet_data$evaluation_table) ==
+              "quantitative_units"
+          ),
+        rows = seq_along(evaluation_sheet_data$evaluation_table$sample) + 1,
+        type = "list",
+        operator = "between", # ignored
+        value =
+          validation_string(
+            input_validation = input_validation,
+            column_vector = quant_units_validation
+          ),
+        allowBlank = TRUE,
+        showInputMsg = TRUE,
+        showErrorMsg = TRUE
+      )
+    )
+  }
+  ### Datetime Observation -----------------------------------------------------
+  openxlsx::dataValidation(
+    wb = workbook,
+    sheet = "evaluation_data",
+    cols =
+      which(
+        colnames(evaluation_sheet_data$evaluation_table) ==
+          "datetime_observation"
+      ),
+    rows = seq_along(evaluation_sheet_data$evaluation_table$sample) + 1,
+    type = "time",
+    operator = "between",
+    value =
+      c(
+        # We probably don't want observations in the ludicrous past of future.
+        # Let's start at Unix time, because, why not?
+        lubridate::ymd_hms("1970-01-01 00:00:00"),
+        # And anything entered in a future century is probably invalid as
+        # well. I mean, if this becomes a problem, this and Excel have some
+        # staying power!
+        lubridate::ymd_hms("2100-01-01 00:00:00")
+      ),
+    allowBlank = TRUE,
+    showInputMsg = TRUE,
+    showErrorMsg = TRUE
+  )
+  ## Apply Styles --------------------------------------------------------------
+  openxlsx::addStyle(
+    wb = workbook,
+    sheet = "input_validation",
+    style = style_locked_gray,
+    rows = c(1:longest) + 1,
+    cols = seq_along(colnames(input_validation)),
+    gridExpand = TRUE
+  )
+  # Header
+  openxlsx::addStyle(
+    wb = workbook,
+    sheet = "input_validation",
+    style = style_header,
+    rows = 1,
+    cols = seq_along(colnames(input_validation)),
+    gridExpand = TRUE
+  )
+  openxlsx::setColWidths(
+    wb = workbook,
+    sheet = "input_validation",
+    cols = seq_along(colnames(input_validation)),
+    widths = "auto"
+  )
+  openxlsx::protectWorksheet(
+    wb = workbook,
+    sheet = "input_validation",
+    protect = TRUE,
+    lockSelectingLockedCells = FALSE,
+    lockSelectingUnlockedCells = FALSE,
+    lockFormattingCells = TRUE,
+    lockFormattingColumns = FALSE,
+    lockFormattingRows = FALSE,
+    lockInsertingColumns = TRUE,
+    lockInsertingRows = TRUE,
+    lockInsertingHyperlinks = FALSE,
+    lockDeletingColumns = TRUE,
+    lockDeletingRows = TRUE,
+    lockSorting = TRUE,
+    lockAutoFilter = TRUE,
+    lockPivotTables = TRUE,
+    lockObjects = TRUE,
+    lockScenarios = TRUE
+  )
   # Workbook protection and saving ---------------------------------------------
   openxlsx::protectWorkbook(
     wb = workbook,
