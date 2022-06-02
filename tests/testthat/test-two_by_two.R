7bcb90e48199f6899b2584f4bfed05bf28c87e0f
Brendan O'Leary <brendan@boleary.com>
2022-06-01 22:10:41 -0400

    Add a test

--- tests/testthat/test-two_by_two.R
+++ tests/testthat/test-two_by_two.R
@@ -1,3 +1,18 @@
 test_that("two_by_two works", {
-  skip("TODO")
+  expect_equal(
+    object =
+      two_by_two(
+        panel_data = nci_1,
+        evaluation_data = test_eval
+      ),
+    expected =
+      tibble::tribble(
+        ~`SARS-COV-2 ELISA (IgG)`, ~`IgG+`, ~`IgG-`, ~`Total`,
+        "IgG+", 27L, NA_integer_, 27L,
+        "IgG Borderline", 2L, NA_integer_, 2L,
+        "IgG-", 1L, 80L, 81L,
+        "Total", 30L, 80L, 110L
+      ),
+    ignore_attr = TRUE
+  )
 })
